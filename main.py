ebbed4f4c27977815c8fec51c71bb135b8757482
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-10-14 15:44:44 -0300

    Catching size of word and showing letters used

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ hangman = [
     ['  ',' ',''],
     [' ',' ',' '],
 ] # enforcado
+used = [] # tentativas
+word='' # letra inserida pelo usuário
 
 # Coloca um "-" para c/ caractere
 for term in choosen:
@@ -23,15 +25,17 @@ def reset():
     global gibbet
     global attempt
     global hangman
+    global used
     choosen = api.getData()   # palavra escolhida
     gibbet=[]   # forca
     attempt = 0 # tentativas
     hangman = [
-        ['  ',' ',''],
+        ['  ','',''],
         [' ',' ',' '],
         ['  ',' ',''],
         [' ',' ',' '],
     ] # enforcado
+    used = [] # tentativas
     # Coloca um "-" para c/ caractere
     for term in choosen:
         gibbet.append('-')
@@ -40,8 +44,6 @@ def reset():
 # Cria a forca e os dashs
 def screen():
     global hangman
-    # Limpa a tela se for no Win
-    os.system('cls') or None
     print(' ________\n'+
          '|        |\n'+
          '|        '+hangman[0][1]+'\n'+
@@ -64,40 +66,51 @@ while stay=='s' or stay=='S':
     # Se não tiver nenhum '-' quer dizer que a palavra está completa
     if '-' not in gibbet:
         print("\nSalvou o Jequetibas, boa!\n")
-        stay=input('Deseja jogar novamente?[s/n]: ')
+        stay=input("\n----------------\nPalavra:"+str(choosen)+"\n----------------\nDeseja jogar novamente?[s/n]: ")
         reset()
     elif attempt==7:
-        print("\nPalavra:"+str(choosen)+"\n\nNãããooo Jequetibas!\nCaixão Lacrado!\n")
+        print("\n----------------\nPalavra:"+str(choosen)+"\n----------------\n\nNãããooo Jequetibas!\nCaixão Lacrado!\n")
         stay=input('Deseja jogar novamente?[s/n]: ')
         reset()
     else:
         # se faltar algum caractere ele monta a tela e solicita a letra
         screen()
+        print("Letras já usadas: "+str(used))
         word=input('Digite uma letra:')
-        
-        # se a letra não se encontrar na palavra escolhida ele registra a tentativa e 
-        #   adiciona uma parte do Jequitibas
-        if word not in choosen:
-            attempt=attempt+1
-            if(attempt==1):
-                hangman[0][1]='O'
-            elif(attempt==2):
-                hangman[1][0]='/'
-            elif(attempt==3):
-                hangman[1][1]='|'
-            elif(attempt==4):
-                hangman[1][2]='\\'
-            elif(attempt==5):
-                hangman[2][1]='|'
-            elif(attempt==6):
-                hangman[3][0]='/'
-            elif(attempt==7):
-                hangman[3][2]='\\'
+        if len(word)>1:
+            # Limpa a tela se for no Win
+            os.system('cls') or None
+            print("\n--------------------\nÉ uma letra só Tião!\n----------------\n")
         else:
-            i=0 # para calcular a posição
-            for term in choosen:
-                # se algum termo for igual a letra digitada pelo usuário, ele altera o "dash" para a letra
-                if term==word:
-                    gibbet[i] = term
-                i=i+1
-            i=0
+            used.append(word)   # lista todas as letras já inseridas
+
+            # se a letra não se encontrar na palavra escolhida ele registra a tentativa e 
+            #   adiciona uma parte do Jequitibas
+            if word not in choosen:
+                attempt=attempt+1
+                if(attempt==1):
+                    hangman[0][1]='O'
+                elif(attempt==2):
+                    hangman[1][0]='/'
+                elif(attempt==3):
+                    hangman[1][1]='|'
+                elif(attempt==4):
+                    hangman[1][2]='\\'
+                elif(attempt==5):
+                    hangman[2][1]='|'
+                elif(attempt==6):
+                    hangman[3][0]='/'
+                elif(attempt==7):
+                    hangman[3][2]='\\'
+                # Limpa a tela se for no Win
+                os.system('cls') or None
+            else:
+                i=0 # para calcular a posição
+                for term in choosen:
+                    # se algum termo for igual a letra digitada pelo usuário, ele altera o "dash" para a letra
+                    if term==word:
+                        gibbet[i] = term
+                    i=i+1
+                i=0
+            # Limpa a tela se for no Win
+            os.system('cls') or None
